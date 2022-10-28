52cc08769d3d077dec41da06dc1a3d08037ab3ce
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-28 16:56:41 +0900

    remove old files

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,13 @@
-import "./style.css";
-import "./components/register-username";
-import "./components/invite-form";
-import "./web-rtc";
-import { WebRTC } from "./web-rtc";
-import { MessageBox } from "./components/message-box";
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-new Promise<string>((resolve) => {
-  const username = window.localStorage.getItem("username");
-  if (username) return resolve(username);
-  window.addEventListener("username-registered", (event) => resolve(event.detail), { once: true });
-}).then((username) => {
-  const webRTC = new WebRTC(username);
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
-  const body = document.querySelector("body")!;
-  body.append(webRTC);
-  const inviteForm = document.querySelector("invite-form")!;
-  inviteForm.myUsername = username;
-  inviteForm.show = true;
-  inviteForm.addEventListener("invitation-sent", (event) => {
-    webRTC.makeOffer(event.detail);
-  });
+if (environment.production) {
+  enableProdMode();
+}
 
-  let messageBox: MessageBox;
-  webRTC.addEventListener("datachannelopen", () => {
-    const { dataChannel } = webRTC;
-    if (!dataChannel) throw TypeError("Data Channel should not be null.");
-    if (messageBox) {
-      body.removeChild(messageBox);
-    }
-    messageBox = new MessageBox(username, dataChannel);
-    body.append(messageBox);
-  });
-});
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
