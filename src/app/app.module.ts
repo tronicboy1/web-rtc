ce536ab04915569d812d81885ba9a8a82b625d42
tronicboy1 <mayeraus@icloud.com>
2022-10-31 06:22:19 +0900

    add swervice worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,24 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AuthComponent } from "./pages/auth/auth.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { ContactsComponent } from './pages/contacts/contacts.component';
-import { CallComponent } from './pages/call/call.component';
+import { ContactsComponent } from "./pages/contacts/contacts.component";
+import { CallComponent } from "./pages/call/call.component";
 import { RtcService } from "@services/rtc.service";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent, AuthComponent, PageNotFoundComponent, ContactsComponent, CallComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: "registerWhenStable:30000",
+    }),
+  ],
   providers: [RtcService],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
