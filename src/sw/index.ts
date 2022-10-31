d0196e7be4b352bbe47961b24b7cef6c536a17c6
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-31 11:46:28 +0900

    get notification on call

--- src/sw/index.ts
+++ src/sw/index.ts
@@ -10,4 +10,8 @@ const callObserver = new CallObserver();
 
 callObserver.watchForInvitations().subscribe((invite) => {
   console.log("SW: Invitation", invite);
+  self.registration.showNotification(`Incoming ${invite.isVideo ? "Video" : "Audio"} Call.`, {
+    body: `Call from ${invite.sender}`,
+    icon: "./assets/icons/icon-192x192.png",
+  });
 });
