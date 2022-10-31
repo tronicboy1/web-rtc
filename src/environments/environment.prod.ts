d52f8f6feb7a8f9a332f03c1221d86ca7d6b2a96
tronicboy1 <mayeraus@icloud.com>
2022-11-01 07:10:59 +0900

    fix build error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true,
+  /** not used in production, but necessary to prevent error */
+  emulatorPorts: {
+    auth: 9099,
+    functions: 5001,
+    firestore: 8080,
+    database: 9000,
+  },
 };
