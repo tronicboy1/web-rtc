bc129667436e443aa7d1a959085482e2d0033477
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-31 12:06:07 +0900

    get url working

--- src/sw/index.ts
+++ src/sw/index.ts
@@ -1,4 +1,5 @@
 import { CallObserver } from "./call-observer";
+import type { CallQueryParameters } from "../app/app-routing.module";
 
 declare var self: ServiceWorkerGlobalScope;
 
@@ -10,8 +11,23 @@ const callObserver = new CallObserver();
 
 callObserver.watchForInvitations().subscribe((invite) => {
   console.log("SW: Invitation", invite);
+  const url = new URL(self.location.origin);
+  url.pathname = "/call";
+  const parameters: CallQueryParameters = { "is-video": Number(invite.isVideo), "their-uid": invite.sender, polite: 0 };
+  Object.entries(parameters).forEach(([key, value]) => {
+    url.searchParams.set(key, String(value));
+  });
+  console.log("SW: URL", url.toString());
   self.registration.showNotification(`Incoming ${invite.isVideo ? "Video" : "Audio"} Call.`, {
     body: `Call from ${invite.sender}`,
     icon: "./assets/icons/icon-192x192.png",
+    data: url.toString(),
   });
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  console.log("SW: Notification Click", event.notification.data);
+  const openWindowPromise = self.clients.matchAll({ type: "window" }).then((windows) => {});
+  event.waitUntil(openWindowPromise);
 });
