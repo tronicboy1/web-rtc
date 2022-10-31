ae5a8d4787519cb61e03f2060cf2aed9a94d2d07
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-10-31 12:11:33 +0900

    get is opened check working

--- src/sw/index.ts
+++ src/sw/index.ts
@@ -17,7 +17,6 @@ callObserver.watchForInvitations().subscribe((invite) => {
   Object.entries(parameters).forEach(([key, value]) => {
     url.searchParams.set(key, String(value));
   });
-  console.log("SW: URL", url.toString());
   self.registration.showNotification(`Incoming ${invite.isVideo ? "Video" : "Audio"} Call.`, {
     body: `Call from ${invite.sender}`,
     icon: "./assets/icons/icon-192x192.png",
@@ -28,6 +27,16 @@ callObserver.watchForInvitations().subscribe((invite) => {
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   console.log("SW: Notification Click", event.notification.data);
-  const openWindowPromise = self.clients.matchAll({ type: "window" }).then((windows) => {});
+  const callUrlString = event.notification.data;
+  if (typeof callUrlString !== "string") throw TypeError("Notification must be url String.");
+  const callUrlObject = new URL(callUrlString);
+  const openWindowPromise = self.clients.matchAll({ type: "window" }).then((windows) => {
+    const alreadyOpened = windows.some((window) => {
+      const windowUrlObject = new URL(window.url);
+      const uidIsEqual = windowUrlObject.searchParams.get("their-uid") === callUrlObject.searchParams.get("their-uid");
+      return uidIsEqual && windowUrlObject.pathname === "/call";
+    });
+    console.log("SW: Notification, already opened?", alreadyOpened);
+  });
   event.waitUntil(openWindowPromise);
 });
