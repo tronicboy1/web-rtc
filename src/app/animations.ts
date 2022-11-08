c324f47d31b795293557e156df91fec858461193
tronicboy1 <mayeraus@icloud.com>
2022-11-08 10:39:16 +0900

    fix animation bug in prod

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,11 +1,7 @@
 import { animate, animateChild, group, query, style, transition, trigger } from "@angular/animations";
-import { AuthComponent } from "./pages/auth/auth.component";
-import { CallComponent } from "./pages/call/call.component";
-import { ChatComponent } from "./pages/chat/chat.component";
-import { ContactsComponent } from "./pages/contacts/contacts.component";
 
-const slideInFromRight = (from: string, to: string) =>
-  transition(`${from} => ${to}`, [
+export const slideInAnimation = trigger("routeAnimations", [
+  transition("ContactsComponent => CallComponent", [
     style({ position: "relative" }),
     query(
       ":enter, :leave",
@@ -26,12 +22,51 @@ const slideInFromRight = (from: string, to: string) =>
       query(":enter", [animate("300ms ease-out", style({ right: "0%" }))], { optional: true }),
       ///query("@*", animateChild()),
     ]),
-  ]);
-
-export const slideInAnimation = trigger("routeAnimations", [
-  slideInFromRight(ContactsComponent.name, ChatComponent.name),
-  slideInFromRight(ContactsComponent.name, CallComponent.name),
-  slideInFromRight(AuthComponent.name, ContactsComponent.name),
+  ]),
+  transition("ContactsComponent => ChatComponent", [
+    style({ position: "relative" }),
+    query(
+      ":enter, :leave",
+      [
+        style({
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+        }),
+      ],
+      { optional: true },
+    ),
+    query(":enter", [style({ right: "-100%" })], { optional: true }),
+    query(":leave", animateChild(), { optional: true }),
+    group([
+      query(":leave", [animate("200ms ease-out", style({ left: "-100%", opacity: 0 }))], { optional: true }),
+      query(":enter", [animate("300ms ease-out", style({ right: "0%" }))], { optional: true }),
+      ///query("@*", animateChild()),
+    ]),
+  ]),
+  transition("AuthComponent => ContactsComponent", [
+    style({ position: "relative" }),
+    query(
+      ":enter, :leave",
+      [
+        style({
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+        }),
+      ],
+      { optional: true },
+    ),
+    query(":enter", [style({ left: "100%" })], { optional: true }),
+    query(":leave", animateChild(), { optional: true }),
+    group([
+      query(":leave", [animate("200ms ease-out", style({ left: "-100%", opacity: 0 }))], { optional: true }),
+      query(":enter", [animate("300ms ease-out", style({ left: "0%" }))], { optional: true }),
+      ///query("@*", animateChild()),
+    ]),
+  ]),
   transition("* <=> *", [
     style({ position: "relative" }),
     query(
