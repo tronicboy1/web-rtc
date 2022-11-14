de9d89a1435931bedf6214cf4505bc69ee24cb0a
tronicboy1 <mayeraus@icloud.com>
2022-11-14 12:34:48 +0900

    fix build bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,11 @@
-import { environment as devEnv } from "./environment";
-
 export const environment = {
   production: true,
   /** not used in production, but necessary to prevent error */
-  emulatorPorts: devEnv.emulatorPorts,
+  emulatorPorts: {
+    auth: 9099,
+    functions: 5001,
+    firestore: 8080,
+    database: 9000,
+    storage: 9199,
+  },
 };
