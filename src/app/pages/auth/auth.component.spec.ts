677794c4b9bb9194fee28fcc86d03e24591667ce
tronicboy1 <mayeraus@icloud.com>
2022-11-27 06:40:45 +0900

    add mock auth service

--- src/app/pages/auth/auth.component.spec.ts
+++ src/app/pages/auth/auth.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { AuthService } from "@services/auth.service";
+import { UserCredential } from "firebase/auth";
 
 import { AuthComponent } from "./auth.component";
+
+class MockAuthService {
+  signInUser(_email: string, _password: string): Promise<void> {
+    return Promise.resolve();
+  }
+  createUser(_email: string, _password: string): Promise<void> {
+    return Promise.resolve();
+  }
+  sendSignInEmail(_email: string): Promise<{}> {
+    return Promise.resolve({});
+  }
+  sendPasswordResetEmail(_email: string): Promise<void> {
+    return Promise.resolve();
+  }
+}
 
 describe("AuthComponent", () => {
   let component: AuthComponent;
@@ -9,6 +26,7 @@ describe("AuthComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AuthComponent],
+      providers: [{ provide: AuthService, useClass: MockAuthService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthComponent);
