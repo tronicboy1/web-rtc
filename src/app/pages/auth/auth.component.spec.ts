3842662a2872c93eaad10e86d81753d57bd32d3b
tronicboy1 <mayeraus@icloud.com>
2022-11-27 07:17:32 +0900

    add some auth tests

--- src/app/pages/auth/auth.component.spec.ts
+++ src/app/pages/auth/auth.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, tick } from "@angular/core/testing";
 import { AuthService } from "@services/auth.service";
 import { UserCredential } from "firebase/auth";
 
@@ -22,6 +22,7 @@ class MockAuthService {
 describe("AuthComponent", () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
+  let nativeElement: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -31,10 +32,39 @@ describe("AuthComponent", () => {
 
     fixture = TestBed.createComponent(AuthComponent);
     component = fixture.componentInstance;
+    nativeElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("Header is displayed", () => {
+    const h1 = nativeElement.querySelector("h1");
+    expect(h1).toBeTruthy();
+    expect(h1?.innerText.length).toBeGreaterThan(0);
+  });
+
+  it("Login mode displays forgot password", () => {
+    const loginModeButton = nativeElement.querySelector<HTMLButtonElement>("button#login-button")!;
+    expect(loginModeButton).toBeTruthy();
+    loginModeButton.click();
+    fixture.detectChanges();
+    const classListArray = Array.from(loginModeButton.classList);
+    expect(classListArray).toContain("active");
+    const forgotPasswordLink = nativeElement.querySelector("a[data-test-id='forgot-password']");
+    expect(forgotPasswordLink).toBeTruthy();
+  });
+
+  it("Register mode can be activated", () => {
+    const registerModeButton = nativeElement.querySelector<HTMLButtonElement>("button#register-button")!;
+    expect(registerModeButton).toBeTruthy();
+    registerModeButton.click();
+    fixture.detectChanges();
+    const classListArray = Array.from(registerModeButton.classList);
+    expect(classListArray).toContain("active");
+    const forgotPasswordLink = nativeElement.querySelector("a[data-test-id='forgot-password']");
+    expect(forgotPasswordLink).toBeFalsy();
   });
 });
